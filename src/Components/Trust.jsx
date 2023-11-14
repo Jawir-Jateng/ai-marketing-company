f6ed9a5ab22adf67aa41fdd38f33de48bf4e5e0d
furqonstudio <furqonstudio@gmail.com>
2023-11-14 21:53:43 +0700

    feat: add Header & Trust Component

---
+++ src/Components/Trust.jsx
@@ -0,0 +1,16 @@
+const Trust = () => {
+  return (
+    <div>
+      <img
+        src="src\assets\trust.png"
+        alt="trust-company"
+        className="object-none m-auto"
+      />
+      <p className="text-center mt-12 text-xs">
+        Trusted by the world’s most ambitious teams.
+      </p>
+    </div>
+  )
+}
+
+export default Trust
